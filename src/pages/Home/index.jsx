f33f88421a7935c9e805ad7f144ce6efddd90437
Alberto <albertolutzdias@gmail.com>
2020-12-07 12:02:01 -0300

    Finalizando layout e slide

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,46 +13,61 @@ import rightArrow from '../../assets/svg/right-arrow.svg';
 
 import NavBar from '../../components/Nav/Navbar';
 
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
-
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
-
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-nextBtn.addEventListener('click', () => {
-    if (counter >= carouselImages.length - 1) return;
-    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-    counter++;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-prevBtn.addEventListener('click', () => {
-    if (counter <= 0) return;
-    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-    counter--;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-carouselSlide.addEventListener('transitionend', () => {
-    if (carouselImages[counter].id === 'lastClone') {
-        carouselSlide.style.transition = "none";
-        counter = carouselImages.length - 2 ;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    }
-    if (carouselImages[counter].id === 'firstClone') {
-        carouselSlide.style.transition = "none";
-        counter = carouselImages.length - counter;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    }
-});
+window.onload=function() {
 
 
 
+let slidePosition = 0;
+const slides = document.getElementsByClassName('carousel__item');
+const totalSlides = slides.length;
+
+
+document.
+  getElementById('nextBtn')
+  .addEventListener("click", function() {
+    moveToNextSlide();
+  });
+document.
+  getElementById('prevBtn')
+  .addEventListener("click", function() {
+    moveToPrevSlide();
+  });
+
+
+
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove('carousel__item--visible');
+    slide.classList.add('carousel__item--hidden');
+  }
+
+  slides[slidePosition].classList.add('carousel__item--visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  updateSlidePosition();
+}
+
+
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  updateSlidePosition();
+
+}
+
+}
 
 function Home() {
     return (
@@ -79,40 +94,45 @@ function Home() {
 
             <div className="carousel">
 
-               
-                    <img src={truckImg} alt="" />
 
-                    <h3>Carousel 1</h3>
+                <img src={truckImg} alt="" />
 
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Vero consequuntur, nulla obcaecati a recusandae amet,
-                        impedit molestiae odit nisi necessitatibus id? Accusantium
-                        architecto dicta delectus.
+                <h3>Carousel 1</h3>
+
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Vero consequuntur, nulla obcaecati a recusandae amet,
+                    impedit molestiae odit nisi necessitatibus id? Accusantium
+                    architecto dicta delectus.
                     </p>
-                </div>
-        
+            </div>
+
 
 
             <div class="carousel-container">
 
-                <div class="carousel-slide">
-                    <img src={thirdImage} id="lastClone" />
-                    <img src={firstImage} />
-                    <img src={secondImagee} />
-                    <img src={thirdImage} />
-                    <img src={firstImage} id="firstClone" />
+                <div class="carousel-slider">
+                    <div class="carousel__item carousel__item--visible">
+                        <img src={firstImage} />
+                    </div>
+                    <div class="carousel__item">
+                        <img src={secondImagee} />
+                    </div>
+                    <div class="carousel__item">
+                        <img src={thirdImage} />
+                    </div>
+
+                   
                 </div>
-
-            </div>
-
-            <button id="prevBtn">
-                <img src={leftArrow} />
-            </button>
-            <button id="nextBtn">
-                <img src={rightArrow} />
-            </button>
-
+                 <div class="carousel__actions">
+                        <button id="prevBtn" aria-label="Previous slide">
+                            <img src={leftArrow} />
+                        </button>
+                        <button id="nextBtn" aria-label="Next slide">
+                            <img src={rightArrow} />
+                        </button>
+                      </div>
+                  </div>
 
         </main>
     );
